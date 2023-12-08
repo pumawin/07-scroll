69e0db2a5c86bf254c1389c1c8cd1aab965a8b82
windy <kjyy2028@gmail.com>
2023-12-08 10:56:22 +0900

    언니로 변경

--- 01-언니가 간다/js/script.js
+++ 01-언니가 간다/js/script.js
@@ -2,16 +2,14 @@ $(function () {
   // 대상을 변수에 저장
   const $window = $(window);
   const $document = $(document);
-  const $progressBar = $('.progress-bar');
+  const $girl = $('.girl');
 
   // 크기를 구해오는 제이쿼리 메서드 : outerHeight()/* 전체크기를 구하는 것이다 */\
   const windowHeight = $window.outerHeight();
   const documentHeight = $document.outerHeight();
-  // console.log(windowHeight, documentHeight);
 
   // scroll영역의 (세로)크기
   const scrollHeight = documentHeight - windowHeight;
-  // console.log(scrollHeight);
 
   // 비율 구하는 공식
   // 대상을 기준으로 나누고 곱하기 100을 하면 된다.
@@ -20,11 +18,9 @@ $(function () {
   $window.on('scroll', function () {
     // 사용자의 (세로)스크롤 값을 구해서
     let scrollTop = $(this).scrollTop();
-    // console.log(scrollTop);
     // 비율을 구하자 공식은, (대상/기준) * 100
     let percent = (scrollTop / scrollHeight) * 100 + '%';
-    console.log(percent);
-    // $progressBar의 width로 적용(%)
-    $progressBar.css('width', percent);
+    // $girl의 left로 적용(%)
+    $girl.css('left', percent);
   });
 });

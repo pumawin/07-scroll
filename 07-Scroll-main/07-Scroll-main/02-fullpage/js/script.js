6d312570c5126fc635642546a4fbd58d9b225dda
windy <kjyy2028@gmail.com>
2023-12-11 12:12:42 +0900

    마우스 휠 방식으로 변경

--- 07-Scroll-main/07-Scroll-main/02-fullpage/js/script.js
+++ 07-Scroll-main/07-Scroll-main/02-fullpage/js/script.js
@@ -3,111 +3,51 @@ $(function () {
   const $window = $(window);
   const $sideDot = $('.indicator > li');
   const $section = $('main > section');
-  // console.log($window, $sideDot, $section);
-
-  let sec1Pos;
-  let sec2Pos;
-  let sec3Pos;
-  let sec4Pos;
-
-  function getPosition() {
-    // 섹션별 y축 위치값 구하기
-    sec1Pos = $section.eq(0).offset().top;
-    sec2Pos = $section.eq(1).offset().top;
-    sec3Pos = $section.eq(2).offset().top;
-    sec4Pos = $section.eq(3).offset().top;
-    console.log(sec1Pos, sec2Pos, sec3Pos, sec4Pos);
-  }
-
   let secIdx = 0;
-  updateDot();
-  getPosition();
 
-  // 스크롤 값 비교하기.
-  $window.on('scroll', function () {
-    let scrollTop = $(this).scrollTop();
-    if (scrollTop >= sec1Pos && scrollTop < sec2Pos) {
-      secIdx = 0;
-    } else if (scrollTop >= sec2Pos && scrollTop < sec3Pos) {
-      secIdx = 1;
-    } else if (scrollTop >= sec3Pos && scrollTop < sec4Pos) {
-      secIdx = 2;
-    } else if (scrollTop >= sec4Pos) {
-      secIdx = 3;
+  // 초기설정
+  moveSection(secIdx);
+
+  // 마우스 휠을 동작시켰을 때
+  $window.on('wheel', function (e) {
+    if ($('html').is(':animated')) return;
+
+    if (e.originalEvent.deltaY < 0) {
+      if (secIdx === 0) return;
+      secIdx--;
+    } else {
+      if (secIdx === 3) return;
+      secIdx++;
     }
 
-    // $sideDot에 적용하기
-    updateDot();
+    moveSection(secIdx);
   });
 
-  function updateDot() {
-    $sideDot.removeClass('on').eq(secIdx).addClass('on');
+  $window.on('resize', function () {
+    moveSection(secIdx);
+  });
+
+  // 영역을 이동시키는 기본 동작
+  function moveSection(index) {
+    const posTop = index * $window.outerHeight();
+    $('html, body').stop().animate(
+      {
+        scrollTop: posTop,
+      },
+      350
+    );
+    updateDot();
   }
 
-  // 인디케이터를 클릭하면 이동
+  // 인디케이터를 클릭했을때
   $sideDot.on('click', function (e) {
     e.preventDefault();
     secIdx = $(this).index();
     moveSection(secIdx);
   });
 
-  function moveSection(index) {
-    $('html, body').animate(
-      {
-        scrollTop: $section.eq(index).offset().top,
-      },
-      400
-    );
+  function updateDot() {
+    $sideDot.removeClass('on');
+    $sideDot.eq(secIdx).addClass('on');
   }
-
-  const $btnTop = $('.btn-top');
-  // TOP버튼을 클릭했을 때
-  $btnTop.on('click', function (e) {
-    e.preventDefault();
-    moveSection(0);
-  });
-
-  // 화면이 리사이징될 때
-  $window.on('resize', function () {
-    // y위치값 다시 계산
-    getPosition();
-    moveSection(secIdx);
-  });
-
-  $window.on('wheel', function (e) {
-    // console.log(e);
-    if ($('html').is(':animated')) {
-      return;
-    }
-    if (e.originalEvent.deltaY < 0) {
-      // 휠을 올린상황
-      // 위로 이동하겠다는 의사표시
-      // 위로 이동하겠다는 건 secIdx값을 줄이는 것, 단 0이하로 줄이면 안 됨.
-      if (secIdx === 0) {
-        return;
-      } else {
-        secIdx--;
-      }
-    } else {
-      // 휠을 내린상황
-      if (secIdx === 3) {
-        return;
-      } else {
-        secIdx++;
-      }
-    }
-    console.log(secIdx);
-    moveSection(secIdx);
-  });
-
-  // // 기본 동작 테스트
-  // // 요소의 y위치값 구하기 : offset().top
-  // let test = $section.eq(2).offset().top;
-
-  // $('html, body').animate(
-  //   {
-  //     scrollTop: test,
-  //   },
-  //   400
-  // );
 });
